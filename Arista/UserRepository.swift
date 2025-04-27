b7c058a0469c22bf0a962fa5f63d06f1d9fe3e75
pascaljesenberger <pascal@jesenberger.fr>
2025-04-27 02:52:39 +0200

    add: UserRepository model

--- Arista/UserRepository.swift
+++ Arista/UserRepository.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import CoreData
+
+struct UserRepository {
+    let viewContext: NSManagedObjectContext
+
+    init(viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
+        self.viewContext = viewContext
+    }
+
+    func getUser() throws -> User? {
+        let request = User.fetchRequest()
+        request.fetchLimit = 1
+        return try viewContext.fetch(request).first
+    }
+}
