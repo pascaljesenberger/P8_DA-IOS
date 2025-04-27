983c9af2a4789c8963622d48a72732bb24dbb8cf
pascaljesenberger <pascal@jesenberger.fr>
2025-04-27 02:56:06 +0200

    add: ExerciseRepository

---
+++ Arista/ExerciseRepository.swift
@@ -0,0 +1,31 @@
+//
+//  ExerciseRepository.swift
+//  Arista
+//
+//  Created by Pascal Jesenberger on 27/04/2025.
+//
+
+import Foundation
+import CoreData
+
+struct ExerciseRepository {
+    let viewContext: NSManagedObjectContext
+    init(viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
+        self.viewContext = viewContext
+    }
+    
+    func getExercise() throws -> [Exercise] {
+        let request = Exercise.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(SortDescriptor<Exercise>(\.startDate, order: .reverse))]
+        return try viewContext.fetch(request)
+    }
+    
+    func addExercise(category: String, duration: Int, intensity: Int, startDate: Date) throws {
+        let newExercise = Exercise(context: viewContext)
+        newExercise.category = category
+        newExercise.duration = Int64(duration)
+        newExercise.intensity = Int64(intensity)
+        newExercise.startDate = startDate
+        try viewContext.save()
+    }
+}
