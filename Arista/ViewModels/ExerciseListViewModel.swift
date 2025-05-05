682d7366044fb13edc84fa326e5f585cd67fa2dc
pascaljesenberger <pascal@jesenberger.fr>
2025-05-05 08:20:21 +0200

    add: use of correct data in exerciselist

--- Arista/ViewModels/ExerciseListViewModel.swift
+++ Arista/ViewModels/ExerciseListViewModel.swift
@@ -6,30 +6,21 @@
 //
 
 import Foundation
-
 import CoreData
 
 class ExerciseListViewModel: ObservableObject {
-    @Published var exercises = [FakeExercise]()
-
+    @Published var exercises = [Exercise]()
     var viewContext: NSManagedObjectContext
-
     init(context: NSManagedObjectContext) {
         self.viewContext = context
         fetchExercises()
     }
-
     private func fetchExercises() {
-        // TODO: fetch data in CoreData and replace dumb value below with appropriate information
-        exercises = [FakeExercise(), FakeExercise(), FakeExercise()]
+        do {
+            let data = ExerciseRepository(viewContext: viewContext)
+            exercises = try data.getExercise()
+        } catch {
+            
+        }
     }
-}
-
-struct FakeExercise: Identifiable {
-    var id = UUID()
-    
-    var category: String = "Football"
-    var duration: Int = 120
-    var intensity: Int = 8
-    var date: Date = Date()
 }
