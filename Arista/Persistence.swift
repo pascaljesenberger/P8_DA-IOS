aeb2b8eddda2e82def5d69f946ba87d2fc7015bf
pascaljesenberger <pascal@jesenberger.fr>
2025-05-05 08:06:55 +0200

    added use of defaultdata in persistence

--- Arista/Persistence.swift
+++ Arista/Persistence.swift
@@ -9,7 +9,7 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
+    
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
@@ -23,9 +23,9 @@ struct PersistenceController {
         }
         return result
     }()
-
+    
     let container: NSPersistentContainer
-
+    
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "Arista")
         if inMemory {
@@ -35,7 +35,6 @@ struct PersistenceController {
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -48,5 +47,7 @@ struct PersistenceController {
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
+        
+        try! DefaultData(viewContext: container.viewContext).apply()
     }
 }
