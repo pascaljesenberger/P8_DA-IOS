e1ca8374f11a05bace12248bdfcd7d9619b1aa25
pascaljesenberger <pascal@jesenberger.fr>
2025-05-05 08:18:21 +0200

    add: use of appropriate data forfetchuserdata

--- Arista/ViewModels/UserDataViewModel.swift
+++ Arista/ViewModels/UserDataViewModel.swift
@@ -11,17 +11,24 @@ import CoreData
 class UserDataViewModel: ObservableObject {
     @Published var firstName: String = ""
     @Published var lastName: String = ""
-
+    
     private var viewContext: NSManagedObjectContext
-
+    
     init(context: NSManagedObjectContext) {
         self.viewContext = context
         fetchUserData()
     }
-
+    
+    
     private func fetchUserData() {
-        // TODO: fetch data in CoreData and replace dumb value below with appropriate information
-        firstName = "Charlotte"
-        lastName = "Corino"
+        do {
+            guard let user = try UserRepository().getUser() else {
+                fatalError()
+            }
+            firstName = user.firstName ?? ""
+            lastName = user.lastName ?? ""
+        } catch {
+            
+        }
     }
 }
